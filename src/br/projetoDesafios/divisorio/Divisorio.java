12b1ef816e00728999bff2709f3b0013665ff3ff
Loveneykens Philogene <loveneykens@gmail.com>
2021-04-14 02:55:04 -0300

    "Divisorio "

--- src/br/projetoDesafios/divisorio/Divisorio.java
+++ src/br/projetoDesafios/divisorio/Divisorio.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Divisorio {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
